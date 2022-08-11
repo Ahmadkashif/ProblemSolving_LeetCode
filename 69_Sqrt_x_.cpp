131de3cf31b29ed59294775ffa644104fcada7c7
Ahmadkashif <ahmadkashif983@gmail.com>
2022-08-11 11:19:46 +0500

    Leetcode revision 69

--- 69_Sqrt_x_.cpp
+++ 69_Sqrt_x_.cpp
@@ -1,14 +1,44 @@
-class Solution {
+// class Solution {
+// public:
+//     long getSqr(long n, int x){
+//         if( (n*n) == x)
+//             return n;
+//         if( (n*n) > x)
+//             return n-1;
+
+//         return getSqr(++n,x);
+//     }
+//     int mySqrt(int x) {
+//         return getSqr(1,x);
+//     }
+// }
+
+
+#include <numeric>
+
+class Solution
+{
 public:
-    long getSqr(long n, int x){
-        if( (n*n) == x)
-            return n;
-        if( (n*n) > x)
-            return n-1;
+    int mySqrt(int x)
+    {
+        if (x < 2)
+            return x;
         
-        return getSqr(++n,x);
+        long long s = 0, e = x, mid;
+
+        while (s <= e)
+        {
+            mid = (e - s) / 2 + s;
+            if(mid * mid == x){
+                return mid;
+            }
+            else if(mid*mid<x){
+                s = mid + 1;
+            }
+            else{
+                e = mid - 1;
+            }
+        }
+        return ( s == 0 ? s : s - 1);
     }
-    int mySqrt(int x) {
-        return getSqr(1,x);
-    }
-}
+};

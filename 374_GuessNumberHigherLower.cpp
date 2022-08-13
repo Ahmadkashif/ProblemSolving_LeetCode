09291828ba0a5adb6f45cce2f92750bc84718a43
Ahmadkashif <ahmadkashif983@gmail.com>
2022-08-13 23:03:55 +0500

    revision leetcode 374

--- 374_GuessNumberHigherLower.cpp
+++ 374_GuessNumberHigherLower.cpp
@@ -1,4 +1,4 @@
-/** 
+/**
  * Forward declaration of guess API.
  * @param  num   your guess
  * @return 	     -1 if num is higher than the picked number
@@ -7,18 +7,38 @@
  * int guess(int num);
  */
 
-class Solution {
+// class Solution {
+// public:
+//     int guessIf(int n){
+//         if(guess(n) == 0)
+//             return n;
+//         else if(guess(n) == - 1)
+//             return guessIf(--n);
+//        else
+//         return guessIf(++n);
+
+//     }
+//     int guessNumber(int n) {
+//         return guessIf(n);
+//     }
+// };
+
+class Solution
+{
 public:
-    int guessIf(int n){
-        if(guess(n) == 0)
-            return n;
-        else if(guess(n) == - 1)
-            return guessIf(--n);
-       else
-        return guessIf(++n);
-        
-    }
-    int guessNumber(int n) {
-        return guessIf(n);
+    int guessNumber(int n)
+    {
+        int s = 1, e = n, mid;
+        while (s < e)
+        {
+            mid = (e - s) / 2 + s;
+            if (guess(mid) == 0)
+                return mid;
+            else if (guess(mid) == 1)
+                s = mid + 1;
+            else
+                e = mid - 1;
+        }
+        return s;
     }
 };

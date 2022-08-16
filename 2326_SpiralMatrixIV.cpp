a8e636731d0e37c396f08f27f81a66c0de086e1c
Ahmadkashif <ahmadkashif983@gmail.com>
2022-08-16 20:50:42 +0500

    leetcode 2326

--- 2326_SpiralMatrixIV.cpp
+++ 2326_SpiralMatrixIV.cpp
@@ -11,6 +11,16 @@
 class Solution {
 public:
     vector<vector<int>> spiralMatrix(int m, int n, ListNode* head) {
-        
-    } 
+        vector<vector<int>> v(m, vector<int> (n, -1));
+        int i=0, j=0, t=0, l=0, b=m-1, r=n-1; //t=top, l=left, b=bottom, r=right
+        while(head) {
+            v[i][j]=head->val;
+            head=head->next;
+            if(i == t && j < r) {j++; if(j==r) t++;}  // fill top most unfilled row 
+            else if(j == r && i < b) {i++; if(i==b) r--;} // fill right most unfilled column
+            else if(i == b && j > l) {j--; if(j==l) b--;} // fill bottom most unfilled row
+            else if(j == l && i > t) {i--; if(i==t) l++;} // fill left most unfilled column
+        }
+        return v;
+    }
 };

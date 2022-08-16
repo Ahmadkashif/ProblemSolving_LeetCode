790187f64f440bd5f8a9c2423d404540ba83b37f
Ahmadkashif <ahmadkashif983@gmail.com>
2022-08-16 19:51:36 +0500

    leetcode 2326

---
+++ 2326_SpiralMatrixIV.cpp
@@ -0,0 +1,16 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> spiralMatrix(int m, int n, ListNode* head) {
+        
+    }
+};

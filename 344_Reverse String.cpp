e604c638ca36d79bfdd6f4b2e5a7c80f52827e95
Ahmadkashif <ahmadkashif983@gmail.com>
2022-08-18 23:33:06 +0500

    leetcode 344

---
+++ 344_Reverse String.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    char getChar(vector<char>& s){
+        if( s.size() == 1){
+            char c = s.front();
+            // s.erase(s.begin());
+            return c;
+        }
+        else{
+            char c = s[0];
+            s.erase(s.begin());
+            getChar(s);
+            s.push_back(c);
+            return c;
+        }
+    }
+    void reverseString(vector<char>& s) {
+        getChar(s);
+    }
+};

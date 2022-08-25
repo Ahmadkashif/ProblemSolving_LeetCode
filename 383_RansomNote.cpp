4f3174be80bfd18c0bf77bda4507e980fc351fc3
Ahmadkashif <ahmadkashif983@gmail.com>
2022-08-25 22:39:52 +0500

    Leetcode 383

---
+++ 383_RansomNote.cpp
@@ -0,0 +1,7 @@
+approach 1:
+    use hashmap for string2
+    construct a map for all 26 letters and their frequency on string one
+    then iterate over string 1 and deduct all frequencies of letters one by one if found, 
+    if not found simply return false
+    else continue;
+    

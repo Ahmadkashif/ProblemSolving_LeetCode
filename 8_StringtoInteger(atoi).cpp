ee9a24ad014d33033994b1c21ee414429587aeec
Ahmadkashif <ahmadkashif983@gmail.com>
2022-08-31 23:59:34 +0500

    Leetcode 8

--- 8_StringtoInteger(atoi).cpp
+++ 8_StringtoInteger(atoi).cpp
@@ -1,31 +1,36 @@
-class Solution {
+class Solution
+{
 public:
-    int myAtoi(string s) {
-        
+    int myAtoi(string s)
+    {
+
         // helper variables
-        int res=0;
-        int i=0;
-        int sign=1;
-		
-        while(i<s.size()&&s[i]==' ')i++;  //ignore leading white space
-        
-        if(s[i]=='-'||s[i]=='+')          //check if number positve or negative
+        int res = 0;
+        int i = 0;
+        int sign = 1;
+
+        while (i < s.size() && s[i] == ' ')
+            i++; // ignore leading white space
+
+        if (s[i] == '-' || s[i] == '+') // check if number positve or negative
         {
-            sign=s[i]=='-'?-1:1;
+            sign = s[i] == '-' ? -1 : 1;
             i++;
         }
         // now iterate across digits if any
-		// should only be in range 0-9
-        while(i<s.length()&&(s[i]>='0'&&s[i]<='9'))  //traverse string till nondigit not found or string ends
+        // should only be in range 0-9
+        while (i < s.length() && (s[i] >= '0' && s[i] <= '9')) // traverse string till nondigit not found or string ends
         {
-            int digit=(s[i]-'0')*sign;
-            if(sign==1 && (res>INT_MAX/10 || (res==INT_MAX/10 && digit>INT_MAX%10))) return INT_MAX; //check for overflow
-            if(sign==-1 &&(res<INT_MIN/10 || (res==INT_MIN/10 && digit<INT_MIN%10))) return INT_MIN; //check for underflow
-            
-            res=res*10+digit; // update res
+            int digit = (s[i] - '0') * sign;
+            if (sign == 1 && (res > INT_MAX / 10 || (res == INT_MAX / 10 && digit > INT_MAX % 10)))
+                return INT_MAX; // check for overflow
+            if (sign == -1 && (res < INT_MIN / 10 || (res == INT_MIN / 10 && digit < INT_MIN % 10)))
+                return INT_MIN; // check for underflow
+
+            res = res * 10 + digit; // update res
             i++;
         }
-    
-    return res;
+
+        return res;
     }
 };

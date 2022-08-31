1d06df1c54c07c74cbae7b5c76173cc2c3846903
Ahmadkashif <ahmadkashif983@gmail.com>
2022-08-31 23:59:22 +0500

    Leetcode 8

--- 8_StringtoInteger(atoi).cpp
+++ 8_StringtoInteger(atoi).cpp
@@ -1,20 +1,31 @@
 class Solution {
 public:
     int myAtoi(string s) {
-        long result = 0;
-    int indicator = 1;
-    for(int i = 0; i<str.size();)
-    {
-        i = str.find_first_not_of(' ');
-        if(str[i] == '-' || str[i] == '+')
-            indicator = (str[i++] == '-')? -1 : 1;
-        while('0'<= str[i] && str[i] <= '9') 
+        
+        // helper variables
+        int res=0;
+        int i=0;
+        int sign=1;
+		
+        while(i<s.size()&&s[i]==' ')i++;  //ignore leading white space
+        
+        if(s[i]=='-'||s[i]=='+')          //check if number positve or negative
         {
-            result = result*10 + (str[i++]-'0');
-            if(result*indicator >= INT_MAX) return INT_MAX;
-            if(result*indicator <= INT_MIN) return INT_MIN;                
+            sign=s[i]=='-'?-1:1;
+            i++;
         }
-        return result*indicator;
-    }
+        // now iterate across digits if any
+		// should only be in range 0-9
+        while(i<s.length()&&(s[i]>='0'&&s[i]<='9'))  //traverse string till nondigit not found or string ends
+        {
+            int digit=(s[i]-'0')*sign;
+            if(sign==1 && (res>INT_MAX/10 || (res==INT_MAX/10 && digit>INT_MAX%10))) return INT_MAX; //check for overflow
+            if(sign==-1 &&(res<INT_MIN/10 || (res==INT_MIN/10 && digit<INT_MIN%10))) return INT_MIN; //check for underflow
+            
+            res=res*10+digit; // update res
+            i++;
+        }
+    
+    return res;
     }
 };

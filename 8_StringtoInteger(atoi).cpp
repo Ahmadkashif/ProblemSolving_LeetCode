838475b9feb365a791cda7f2638b05fa57595fbc
Ahmadkashif <ahmadkashif983@gmail.com>
2022-08-31 23:58:45 +0500

    Leetcode 8

---
+++ 8_StringtoInteger(atoi).cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int myAtoi(string s) {
+        long result = 0;
+    int indicator = 1;
+    for(int i = 0; i<str.size();)
+    {
+        i = str.find_first_not_of(' ');
+        if(str[i] == '-' || str[i] == '+')
+            indicator = (str[i++] == '-')? -1 : 1;
+        while('0'<= str[i] && str[i] <= '9') 
+        {
+            result = result*10 + (str[i++]-'0');
+            if(result*indicator >= INT_MAX) return INT_MAX;
+            if(result*indicator <= INT_MIN) return INT_MIN;                
+        }
+        return result*indicator;
+    }
+    }
+};

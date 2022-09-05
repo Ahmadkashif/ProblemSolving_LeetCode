3d29c3b6d3a5d038d6e593d8aa988c947dabfbc8
Ahmadkashif <ahmadkashif983@gmail.com>
2022-09-05 23:23:09 +0500

    Leetcode 859 1909

--- 1909_RemoveOneElementtoMaketheArrayStrictlyIncreasing.cpp
+++ 1909_RemoveOneElementtoMaketheArrayStrictlyIncreasing.cpp
@@ -1,22 +1,26 @@
-class Solution {
+class Solution
+{
 public:
-     bool canBeIncreasing(vector<int>& nums) {
-        
+    bool canBeIncreasing(vector<int> &nums)
+    {
+
         int count = 0;
-        for(int i = 1; i < nums.size(); i++){            
-            
-            if(nums[i] <= nums[i-1]){
-                
-                if(count == 1)
+        for (int i = 1; i < nums.size(); i++)
+        {
+
+            if (nums[i] <= nums[i - 1])
+            {
+
+                if (count == 1)
                     return false;
-                
-                if( i >= 2 && nums[i-2] >= nums[i])
-                    nums[i] = nums[i-1];
-                
+
+                if (i >= 2 && nums[i - 2] >= nums[i])
+                    nums[i] = nums[i - 1];
+
                 count++;
-            }            
+            }
         }
-        
-        return count <= 1;        
+
+        return count <= 1;
     }
 };

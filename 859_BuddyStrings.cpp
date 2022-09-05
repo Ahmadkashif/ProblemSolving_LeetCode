4dc28832f7e2e8a8ab776eec4670225e6e47eeba
Ahmadkashif <ahmadkashif983@gmail.com>
2022-09-05 23:20:35 +0500

    Leetcode 859

---
+++ 859_BuddyStrings.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+     bool buddyStrings(string A, string B) {
+        // Same length
+        int len_A = A.size(), len_B = B.size();
+        if (len_A != len_B) return false;
+        
+        // Repeat: same string, A needs repeated char, like "aab" "aab"
+        int numChar_A = unordered_set<char>(A.begin(), A.end()).size();
+        if (A == B && numChar_A < len_B) return true;
+        
+        // Swap: There should be only two to change
+        vector<int> index_diff;
+                        
+        for (int i = 0; i < len_A; i++) {
+          if (A[i] != B[i]) index_diff.push_back(i);
+          if (index_diff.size() > 2) return false;
+        } 
+        
+        return index_diff.size() == 2 &&
+               A[index_diff[0]] == B[index_diff[1]] &&
+               A[index_diff[1]] == B[index_diff[0]];        
+    }
+};

e6b10802806cf1e6472abf9e13a46ceba137831d
Ahmadkashif <ahmadkashif983@gmail.com>
2022-09-12 19:28:12 +0500

    AE validate subsequence

---
+++ AE_ValidateSubsequence.cpp
@@ -0,0 +1,19 @@
+#include <stack>
+
+using namespace std;
+
+bool isValidSubsequence(vector<int> arr, vector<int> seq) {
+
+  stack<int> stack;
+  for(int i = seq.size()-1; i >= 0 ; i--)
+    stack.push(seq[i]);
+
+  for(int n : arr){
+    if(stack.top() == n){   
+      stack.pop();
+      if(stack.empty())
+        return true;
+    }
+  }
+  return false;
+}

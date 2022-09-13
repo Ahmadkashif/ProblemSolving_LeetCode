7016e0f469cbae68b9d4a0dc57d638c23cb09e12
Ahmadkashif <ahmadkashif983@gmail.com>
2022-09-13 23:42:25 +0500

    AE sorted squared array

---
+++ AE_SortedSquaredArray.cpp
@@ -0,0 +1,10 @@
+#include <vector>
+using namespace std;
+
+vector<int> sortedSquaredArray(vector<int> array) {
+  vector<int> res;
+  for(int n: array)
+    res.push_back(n*n);
+  sort(res.begin(), res.end());
+  return res;
+}

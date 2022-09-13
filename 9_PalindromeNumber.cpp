2e43a4603c0e2f30765690d542a19a6767983916
Ahmadkashif <ahmadkashif983@gmail.com>
2022-09-13 23:45:12 +0500

    Leetcode 9

--- 9_PalindromeNumber.cpp
+++ 9_PalindromeNumber.cpp
@@ -1,13 +1,16 @@
-class Solution {
+class Solution
+{
 public:
-    bool isPalindrome(int x) {
-        if(x<0|| (x!=0 &&x%10==0)) return false;
-        int sum=0;
-        while(x>sum)
+    bool isPalindrome(int x)
+    {
+        if (x < 0 || (x != 0 && x % 10 == 0))
+            return false;
+        int sum = 0;
+        while (x > sum)
         {
-            sum = sum*10+x%10;
-            x = x/10;
+            sum = sum * 10 + x % 10;
+            x = x / 10;
         }
-        return (x==sum)||(x==sum/10);
+        return (x == sum) || (x == sum / 10);
     }
 };

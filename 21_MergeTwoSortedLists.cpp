55aca2efd6b7892b481ff8077c550ff04ea05e99
Ahmadkashif <ahmadkashif983@gmail.com>
2022-09-14 22:39:32 +0500

    Leetcode 21

---
+++ 21_MergeTwoSortedLists.cpp
@@ -0,0 +1,16 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        
+    }
+};

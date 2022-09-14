d001b7bff273138f95b1019ddb68657c0745de26
Ahmadkashif <ahmadkashif983@gmail.com>
2022-09-14 22:41:53 +0500

    Leetcode 21

--- 21_MergeTwoSortedLists.cpp
+++ 21_MergeTwoSortedLists.cpp
@@ -1,29 +1,30 @@
-class Solution {
+class Solution
+{
 public:
-	ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) 
-  {
-		// if list1 happen to be NULL
-		if(l1 == NULL)
+    ListNode *mergeTwoLists(ListNode *l1, ListNode *l2)
+    {
+        // if list1 happen to be NULL
+        if (l1 == NULL)
         {
-			return l2;
-		}
-		
-		// if list2 happen to be NULL
-		if(l2 == NULL)
+            return l2;
+        }
+
+        // if list2 happen to be NULL
+        if (l2 == NULL)
         {
-			return l1;
-		} 
-		
-		// if value pointend by l1 pointer is less than equal to value pointed by l2 pointer
-		if(l1 -> val <= l2 -> val)
+            return l1;
+        }
+
+        // if value pointend by l1 pointer is less than equal to value pointed by l2 pointer
+        if (l1->val <= l2->val)
         {
-			l1 -> next = mergeTwoLists(l1 -> next, l2);
-			return l1;
-		}
-		else
+            l1->next = mergeTwoLists(l1->next, l2);
+            return l1;
+        }
+        else
         {
-			l2 -> next = mergeTwoLists(l1, l2 -> next);
-			return l2;            
-		}
-	}
-};	
+            l2->next = mergeTwoLists(l1, l2->next);
+            return l2;
+        }
+    }
+};

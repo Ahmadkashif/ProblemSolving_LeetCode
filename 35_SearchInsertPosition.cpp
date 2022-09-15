55d3d49c64b36d72e76770c84bb59c864c8890d4
Ahmadkashif <ahmadkashif983@gmail.com>
2022-09-15 22:57:58 +0500

    Leetcode 35

--- 35_SearchInsertPosition.cpp
+++ 35_SearchInsertPosition.cpp
@@ -3,10 +3,9 @@ class Solution
 public:
     int searchInsert(vector<int> &nums, int t)
     {
-        // if( nums.size() == 1) return nums.size()-1;
-        int sIndex = 0,               // 0 1
-            eIndex = nums.size() - 1, // 3 1
-            mIndex;                   // 1 0
+        int sIndex = 0,               
+            eIndex = nums.size() - 1, 
+            mIndex;                   
         while (sIndex < eIndex)
         {
             mIndex = (eIndex - sIndex) / 2 + sIndex;

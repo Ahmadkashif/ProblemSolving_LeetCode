d57accb1ae3996966124c1a8102d25d3f16a79e7
Ahmadkashif <ahmadkashif983@gmail.com>
2022-09-16 19:38:39 +0500

    Leetcode 153 and AE_3

---
+++ 153_FindMinimumInRotatedSortedArray.cpp
@@ -0,0 +1,27 @@
+class Solution
+{
+public:
+    int findMin(vector<int> &nums)
+    {
+
+        int s = 0, e = nums.size() - 1, mid;
+
+        while (s < e)
+        {
+            mid = (e - s) / 2 + s;
+            // if(nums[s] > nums[mid] && nums[mid] < nums[e])
+            //     return nums[mid];
+            if (nums[s] > nums[mid])
+            {
+                e = mid;
+            }
+            else if (nums[mid] > nums[e])
+            {
+                s = mid + 1;
+            }
+            else
+                e = mid - 1;
+        }
+        return nums[s];
+    }
+};
--- AE_SortedSquaredArray.cpp
+++ AE_SortedSquaredArray.cpp
@@ -1,11 +1,47 @@
 #include <vector>
 using namespace std;
 
+// Solution 1
 vector<int> sortedSquaredArray(vector<int> array)
 {
     vector<int> res;
     for (int n : array)
         res.push_back(n * n);
     sort(res.begin(), res.end());
+    return res;
+}
+
+// Solution 2
+#include <vector>
+using namespace std;
+
+vector<int> sortedSquaredArray(vector<int> array)
+{
+    int n = array.size() - 1;
+    vector<int> res(array.size());
+    int *lPtr, *rPtr;
+    lPtr = &array[0];
+    rPtr = &array[array.size() - 1];
+
+    for (int i = 0; i < array.size(); i++)
+    {
+        int x = *lPtr, y = *rPtr;
+        if (x * x > y * y)
+        {
+            res[n - i] = x * x;
+            lPtr++;
+        }
+        else if (x * x < y * y)
+        {
+            res[n - i] = y * y;
+            rPtr--;
+        }
+        else
+        {
+            res[n - i] = x * x;
+            lPtr++;
+        }
+    }
+
     return res;
 }

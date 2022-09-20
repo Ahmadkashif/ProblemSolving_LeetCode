82e93a7c96f8045767932309e72cc3639d4c9faf
Ahmadkashif <ahmadkashif983@gmail.com>
2022-09-20 13:21:43 +0500

    AE tournament winner

--- AE_TournamentWinner.cpp
+++ AE_TournamentWinner.cpp
@@ -8,12 +8,12 @@ string tournamentWinner(vector<vector<string>> competitions, vector<int> results
     for (int i = 0; i < competitions.size(); i++)
     {
         if (results[i])
-            m[arr[0]] += 1;
+            m[competitions[i][0]] += 1;
         else
-            m[arr[1]] += 1;
+            m[competitions[i][1]] += 1;
     }
     vector<string> res(m.size());
-    for (map<string, int> iter i = m.begin(); i < m.end(); ++iter)
+    for (map<string, int>::iterator iter = m.begin(); i != m.end(); ++iter)
     {
         res[iter->second] = iter->first;
     }

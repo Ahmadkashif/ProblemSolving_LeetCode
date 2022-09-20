74235edbb46bd3b4f9f740012918b5db2734f065
Ahmadkashif <ahmadkashif983@gmail.com>
2022-09-20 13:35:47 +0500

    AE tournament winner

--- AE_TournamentWinner.cpp
+++ AE_TournamentWinner.cpp
@@ -5,6 +5,8 @@ using namespace std;
 string tournamentWinner(vector<vector<string>> competitions, vector<int> results)
 {
     map<string, int> m;
+    string answer;
+    int bestScore = 0;
     for (int i = 0; i < competitions.size(); i++)
     {
         if (results[i])
@@ -12,11 +14,15 @@ string tournamentWinner(vector<vector<string>> competitions, vector<int> results
         else
             m[competitions[i][1]] += 1;
     }
-    vector<string> res(m.size());
-    for (map<string, int>::iterator iter = m.begin(); i != m.end(); ++iter)
+
+    for (map<string, int>::iterator iter = m.begin(); iter != m.end(); ++iter)
     {
-        res[iter->second] = iter->first;
+        if (iter->second > bestScore)
+        {
+            bestScore = iter->second;
+            answer = iter->first;
+        }
     }
 
-    return res[res.size() - 1];
+    return answer;
 }

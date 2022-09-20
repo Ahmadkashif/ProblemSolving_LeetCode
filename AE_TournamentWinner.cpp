94f28a3f2cb575274c0558fd41e4a75b21c5267c
Ahmadkashif <ahmadkashif983@gmail.com>
2022-09-20 13:11:11 +0500

    AE tournament winner

---
+++ AE_TournamentWinner.cpp
@@ -0,0 +1,15 @@
+#include <vector>
+#include <map>
+using namespace std;
+
+string tournamentWinner(vector<vector<string>> competitions, vector<int> results)
+{
+    map<string, int> m;
+    for (int i = 0; i < competitions.size(); i++)
+    {
+        if (results[i])
+            m[arr[0]] += 1;
+    }
+    
+    return "";
+}

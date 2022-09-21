2e0cc3c09a56f8f2215b82ff09a605c53f650348
Ahmadkashif <ahmadkashif983@gmail.com>
2022-09-21 23:14:22 +0500

    Leetcode 561

--- 561_ArrayPartition.cpp
+++ 561_ArrayPartition.cpp
@@ -1,16 +1,20 @@
-class Solution {
+class Solution
+{
 public:
-    int min(int a, int b){
-        return (a<=b?a:b);
+    int min(int a, int b)
+    {
+        return (a <= b ? a : b);
     }
-    int arrayPairSum(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
+    int arrayPairSum(vector<int> &nums)
+    {
+        sort(nums.begin(), nums.end());
         int sum = 0;
         int *ptr1, *ptr2;
-        for(int i = 0; i < nums.size(); i+=2){
+        for (int i = 0; i < nums.size(); i += 2)
+        {
             ptr1 = &nums[i];
-            ptr2 = &nums[i+1];
-            sum += min(*ptr1,*ptr2);
+            ptr2 = &nums[i + 1];
+            sum += min(*ptr1, *ptr2);
         }
         return sum;
     }

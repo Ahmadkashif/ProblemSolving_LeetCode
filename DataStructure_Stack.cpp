22abb5c5557699e9cc2d1a069dcc78260516d033
Ahmadkashif <ahmadkashif983@gmail.com>
2022-09-21 23:24:18 +0500

    DS Stack

--- DataStructure_Stack.cpp
+++ DataStructure_Stack.cpp
@@ -121,23 +121,23 @@ int main()
         cout << "Stack is not Empty mere dost " << endl;
     }
 
-    /*
-    //creation of stack
-    stack<int> s;
-    //push operation
-    s.push(2);
-    s.push(3);
-    //pop
-    s.pop();
-    cout << "Printing top element " << s.top() << endl;
-    if(s.empty()){
-        cout << "Stack is empty " << endl;
-    }
-    else{
-        cout << "stack is not empty " << endl;
-    }
-    cout << "size of stack is " << s.size() << endl;
-    */
+    // /*
+    // //creation of stack
+    // stack<int> s;
+    // //push operation
+    // s.push(2);
+    // s.push(3);
+    // //pop
+    // s.pop();
+    // cout << "Printing top element " << s.top() << endl;
+    // if(s.empty()){
+    //     cout << "Stack is empty " << endl;
+    // }
+    // else{
+    //     cout << "stack is not empty " << endl;
+    // }
+    // cout << "size of stack is " << s.size() << endl;
+    // */
 
     return 0;
 }

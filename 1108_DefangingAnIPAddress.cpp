ebe41e9ba25367a9b0044875dc980b85426371bc
Ahmadkashif <ahmadkashif983@gmail.com>
2022-09-27 23:33:22 +0500

    leetcode 1108

--- 1108_DefangingAnIPAddress.cpp
+++ 1108_DefangingAnIPAddress.cpp
@@ -1,13 +1,9 @@
 class Solution {
-    public String defangIPaddr(String address) {
-          StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < address.length(); i++) {
-            if (address.charAt(i) == '.') {
-                sb.append("["+address.charAt(i)+"]");
-            }
-            else
-                sb.append(address.charAt(i));
-        }
-        return sb.toString();
-  }
-}
+public:
+    string defangIPaddr(string address) {
+        for (int i = address.size() - 2; i >= 0; i--)
+            if (address[i + 1] == '.')
+                address = address.substr(0, i + 1) + "[.]" + address.substr(i + 2);
+        return address;
+    }
+};

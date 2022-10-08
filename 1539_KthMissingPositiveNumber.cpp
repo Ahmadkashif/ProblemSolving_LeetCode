7b08f587bf0736241b66e2c1b87f0eed2d36d5c8
Ahmadkashif <ahmadkashif983@gmail.com>
2022-10-09 00:10:47 +0500

    leetcode 1539

--- 1539_KthMissingPositiveNumber.cpp
+++ 1539_KthMissingPositiveNumber.cpp
@@ -1,15 +1,18 @@
-class Solution {
+class Solution
+{
 public:
-    int findKthPositive(vector<int>& arr, int k) {
+    int findKthPositive(vector<int> &arr, int k)
+    {
         int maxRange = arr.back() + k;
         vector<int> res;
         int iter = 0;
-        for(int i = 1; i <= maxRange; i++){
-            if(iter < arr.size() && arr[iter] == i)
+        for (int i = 1; i <= maxRange; i++)
+        {
+            if (iter < arr.size() && arr[iter] == i)
                 iter++;
             else
                 res.push_back(i);
         }
-        return res[k-1];
+        return res[k - 1];
     }
 };

37afda9eee4fa9532104ea0e7c363650f7e3c022
Ahmadkashif <ahmadkashif983@gmail.com>
2022-10-10 23:22:07 +0500

    leetcode 1614

--- 1614_MaximumNestingDepthoftheParentheses.cpp
+++ 1614_MaximumNestingDepthoftheParentheses.cpp
@@ -1,16 +1,18 @@
-class Solution {
+class Solution
+{
 public:
-       int maxDepth(string s) {
+    int maxDepth(string s)
+    {
         int res = 0;
         int depth = 0;
-        for(int i =0 ; i < s.size(); ++i)
+        for (int i = 0; i < s.size(); ++i)
         {
-             if(s[i] == '(')
+            if (s[i] == '(')
                 ++depth;
-            else if(s[i] == ')')
+            else if (s[i] == ')')
                 --depth;
             res = max(depth, res);
         }
-    return res;
+        return res;
     }
 };

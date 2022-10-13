87f0a48d3f29a629bcc1c0fd7bceae59c168d740
Ahmadkashif <ahmadkashif983@gmail.com>
2022-10-13 10:16:52 +0500

    leetcode 1662

--- 1662_CheckIfTwoStringArraysareEquivalent.cpp
+++ 1662_CheckIfTwoStringArraysareEquivalent.cpp
@@ -1,11 +1,17 @@
-class Solution {
+class Solution
+{
 public:
-    bool arrayStringsAreEqual(vector<string>& w1, vector<string>& w2) {
+    bool arrayStringsAreEqual(vector<string> &w1, vector<string> &w2)
+    {
         int i2 = 0, j2 = 0, lmt2 = w2.size();
-        for (string chunk1: w1) {
-            for (char c: chunk1) {
-                if (c != w2[i2][j2++] || i2 == lmt2) return false;
-                if (j2 == w2[i2].size()) {
+        for (string chunk1 : w1)
+        {
+            for (char c : chunk1)
+            {
+                if (c != w2[i2][j2++] || i2 == lmt2)
+                    return false;
+                if (j2 == w2[i2].size())
+                {
                     i2++, j2 = 0;
                 }
             }

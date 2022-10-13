0641812a925777caafd2940a2c6e85af009fd800
Ahmadkashif <ahmadkashif983@gmail.com>
2022-10-13 10:17:58 +0500

    leetcode 1662

--- 1662_CheckIfTwoStringArraysareEquivalent.cpp
+++ 1662_CheckIfTwoStringArraysareEquivalent.cpp
@@ -1,21 +1,18 @@
-class Solution
-{
+class Solution {
 public:
-    bool arrayStringsAreEqual(vector<string> &w1, vector<string> &w2)
-    {
+    bool arrayStringsAreEqual(vector<string>& w1, vector<string>& w2) {
+		// support variables
         int i2 = 0, j2 = 0, lmt2 = w2.size();
-        for (string chunk1 : w1)
-        {
-            for (char c : chunk1)
-            {
-                if (c != w2[i2][j2++] || i2 == lmt2)
-                    return false;
-                if (j2 == w2[i2].size())
-                {
+        for (string chunk1: w1) {
+            for (char c: chunk1) {
+                if (i2 == lmt2 || c != w2[i2][j2++]) return false;
+                // updating w2 pointers
+                if (j2 == w2[i2].size()) {
                     i2++, j2 = 0;
                 }
             }
         }
+        // we return if we are commpletely done parsing w2 too
         return i2 == lmt2;
     }
 };

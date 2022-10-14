1429ba483a7a974d87c6a52fc857de82bd06ae30
Ahmadkashif <ahmadkashif983@gmail.com>
2022-10-14 06:46:40 +0500

    AE 5

---
+++ AE_NonConstructibleChange.cpp
@@ -0,0 +1,16 @@
+#include <vector>
+using namespace std;
+
+int nonConstructibleChange(vector<int> coins) {
+
+  sort(coins.begin(), coins.end());
+  int sum = 0;
+
+  for(auto& itr : coins){
+    if(itr>(sum+1))
+      return sum+1;
+    else
+    sum+=itr;
+  }
+  return sum+1;
+}

b8fdd7263c840b078a9c7f213319081a689ddd2a
Ahmadkashif <ahmadkashif983@gmail.com>
2022-10-22 22:34:38 +0500

    leetcode 1672

--- 1672_RichestCustomerWealth.cpp
+++ 1672_RichestCustomerWealth.cpp
@@ -1,12 +1,16 @@
-class Solution {
-    public int maximumWealth(int[][] accounts) {
-                int result = 0;
-        for (int[] a : accounts) {
-            int localMax=0;
-            for (int b : a) {
-                localMax+=b;
+class Solution
+{
+    int maximumWealth(int[][] accounts)
+    {
+        int result = 0;
+        for (int[] a : accounts)
+        {
+            int localMax = 0;
+            for (int b : a)
+            {
+                localMax += b;
             }
-            result = result>localMax? result:localMax;
+            result = result > localMax ? result : localMax;
         }
         return result;
     }

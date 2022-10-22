7b6ba4e7b6714c957fc9dd68d7ef2bca5e6c5b31
Ahmadkashif <ahmadkashif983@gmail.com>
2022-10-22 22:37:15 +0500

    leetcode 1678

--- 1678_GoalParserInterpretation.cpp
+++ 1678_GoalParserInterpretation.cpp
@@ -1,6 +1,16 @@
 class Solution {
-public:
-    string interpret(string command) {
-        
-    }
-};
+    public String interpret(String command) {
+                 char[] arr = command.toCharArray();
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]=='(' && arr[i+1]==')'){
+                sb.append('o');
+            }
+            else if(arr[i]=='(' && arr[i+1]=='a' && arr[i+2]=='l'&& arr[i+3]==')'){
+                sb.append("al");
+            }
+            else if (arr[i]=='G')
+                sb.append("G");
+        }
+        return sb.toString();    }
+}

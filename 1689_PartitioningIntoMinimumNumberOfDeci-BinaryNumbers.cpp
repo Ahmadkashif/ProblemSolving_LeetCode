694408f8b5c05bfb8ea12f62e3c768ecb34010ca
Ahmadkashif <ahmadkashif983@gmail.com>
2022-11-03 23:12:08 +0500

    leetcode 1689

--- 1689_PartitioningIntoMinimumNumberOfDeci-BinaryNumbers.cpp
+++ 1689_PartitioningIntoMinimumNumberOfDeci-BinaryNumbers.cpp
@@ -1,12 +1,15 @@
-class Solution {
+class Solution
+{
 public:
-    int minPartitions(string n) {
-        int r , number = 0, max = -1;
-        int rSize = n.length() - 1 ;
-        
-        for(char c: n){
-            if(max < (c-'0'))
-                max = (c-'0');
+    int minPartitions(string n)
+    {
+        int r, number = 0, max = -1;
+        int rSize = n.length() - 1;
+
+        for (char c : n)
+        {
+            if (max < (c - '0'))
+                max = (c - '0');
         }
         return max;
     }

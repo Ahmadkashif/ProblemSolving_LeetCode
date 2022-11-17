4fdb37a86c17d42c178254beff185b42c23f4257
Ahmadkashif <ahmadkashif983@gmail.com>
2022-11-17 12:16:31 +0500

    leetcode 1929

--- 1929_ConcatenateArray.cpp
+++ 1929_ConcatenateArray.cpp
@@ -13,20 +13,24 @@ using namespace std;
 //     return nums;
 // }
 
-short int size = 0;
-vector<int> getConcatenation(vector<int> &nums)
-{
-    if (size == 0)
-    {
-        size = nums.size();
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        
+        int n = nums.size();
+        vector<int> ans1 (n);
+        vector<int> ans2 (n);
+        // create an array of size 2 x n
+        
+        for (int i = 0; i < n ; i++)
+        {
+            ans1[i] = nums[i];
+        }
+        
+        for ( int i =  0; i < n ; i++ )
+        {
+            ans2[i] = nums[i];
+        }
+        return ans;
     }
-    
-    if (nums.size() == 2 * size)
-        return nums;
-
-    else
-    {
-        nums.push_back(nums[nums.size() - size]);
-        return getConcatenation(nums);
-    }
-}
+};

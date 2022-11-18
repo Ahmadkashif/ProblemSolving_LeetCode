8fecf78f04548800da7c76f8bfd0bb6d26048e11
Ahmadkashif <ahmadkashif983@gmail.com>
2022-11-18 18:02:31 +0500

    leetcode 1480

--- 1480_RunningSumof1dArray.cpp
+++ 1480_RunningSumof1dArray.cpp
@@ -8,17 +8,19 @@ public:
         return nums;
     }
 };
-class Solution {
+class Solution
+{
 public:
-    vector<int> runningSum(vector<int>& nums) {
+    vector<int> runningSum(vector<int> &nums)
+    {
         int sum = 0;
-        
-        for(int i = 0; i < nums.size(); i++)
+
+        for (int i = 0; i < nums.size(); i++)
         {
             sum = sum + nums[i];
             nums[i] = sum;
         }
-        
+
         return nums;
     }
 };

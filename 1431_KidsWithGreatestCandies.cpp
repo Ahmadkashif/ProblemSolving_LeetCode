569bb51164f262402f9421d59bdc2177dd40c5c8
Ahmadkashif <ahmadkashif983@gmail.com>
2022-11-20 21:19:54 +0500

    leetcode 1431

--- 1431_KidsWithGreatestCandies.cpp
+++ 1431_KidsWithGreatestCandies.cpp
@@ -3,31 +3,32 @@
 
 using namespace std;
 
-        int ptr = 0;
+int ptr = 0;
 vector<bool> results;
 
 int getMax(vector<int> &v)
 {
         int max = 0;
-        for (int i = 0; i<v.size();i++)
-                if(max<v[i])
+        for (int i = 0; i < v.size(); i++)
+                if (max < v[i])
                         max = v[i];
-        
-    return max;
+
+        return max;
 }
-    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+vector<bool> kidsWithCandies(vector<int> &candies, int extraCandies)
+{
 
-            if(!(ptr < candies.size()))
-               return results;
-    if ((extraCandies + candies[ptr++]) < getMax(candies))
-    results.push_back(false);
+        if (!(ptr < candies.size()))
+                return results;
+        if ((extraCandies + candies[ptr++]) < getMax(candies))
+                results.push_back(false);
 
-    else 
-    results.push_back(true);
+        else
+                results.push_back(true);
 
-    kidsWithCandies(candies, extraCandies);
-            return results;
-    }
-int main(){
-    
+        kidsWithCandies(candies, extraCandies);
+        return results;
+}
+int main()
+{
 }

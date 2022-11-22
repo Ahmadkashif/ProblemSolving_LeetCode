584471d724af1946c9eaaa4e70ba1694212e851c
Ahmadkashif <ahmadkashif983@gmail.com>
2022-11-22 07:52:03 +0500

    leetcode 1512

--- 1512_NumberOfGoodPairs.cpp
+++ 1512_NumberOfGoodPairs.cpp
@@ -10,24 +10,27 @@ int numIdenticalPairs(vector<int> &nums)
         for (int j = 0; j < nums.size(); j++)
         {
             if (nums[i] == nums[j] && i < j)
-            count++;
+                count++;
         }
 
     return count;
 }
 
-class Solution {
+class Solution
+{
 public:
-    int numIdenticalPairs(vector<int>& nums) {
-        
-        int mem[101] ={0};
-        int sum=0;
-        
-        for(int i=0; i < nums.size(); i++){
+    int numIdenticalPairs(vector<int> &nums)
+    {
+
+        int mem[101] = {0};
+        int sum = 0;
+
+        for (int i = 0; i < nums.size(); i++)
+        {
             sum += mem[nums[i]];
             ++mem[nums[i]];
         }
-        
+
         return sum;
     }
 };

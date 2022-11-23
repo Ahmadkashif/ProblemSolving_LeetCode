cc5b455ab97fbc96b2598cec3cd82dedefe1179d
Ahmadkashif <ahmadkashif983@gmail.com>
2022-11-23 11:53:33 +0500

    leetcode 1832

---
+++ 1832_CheckiftheSentenceIsPangram.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool checkIfPangram(string s) {
+        vector<int> indices(26);
+        
+        for(int i = 0; i < s.length(); i++){
+            indices[ s.at(i) - 'a' ]++;
+        }
+        
+        for(int i = 0; i < indices.size(); i++)
+        {
+            if(indices[i] == 0)
+                break;
+            if(i==indices.size()-1 && indices[i] > 0)
+                return true;
+        }
+        
+        return false;
+    }
+};

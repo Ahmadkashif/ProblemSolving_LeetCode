721d87d2670e15cbba7790c1c89d99e48b1ed2bb
Ahmadkashif <ahmadkashif983@gmail.com>
2022-11-23 12:05:06 +0500

    leetcode 1832

--- 1832_CheckiftheSentenceIsPangram.cpp
+++ 1832_CheckiftheSentenceIsPangram.cpp
@@ -1,20 +1,23 @@
-class Solution {
+class Solution
+{
 public:
-    bool checkIfPangram(string s) {
+    bool checkIfPangram(string s)
+    {
         vector<int> indices(26);
-        
-        for(int i = 0; i < s.length(); i++){
-            indices[ s.at(i) - 'a' ]++;
-        }
-        
-        for(int i = 0; i < indices.size(); i++)
+
+        for (int i = 0; i < s.length(); i++)
         {
-            if(indices[i] == 0)
+            indices[s.at(i) - 'a']++;
+        }
+
+        for (int i = 0; i < indices.size(); i++)
+        {
+            if (indices[i] == 0)
                 break;
-            if(i==indices.size()-1 && indices[i] > 0)
+            if (i == indices.size() - 1 && indices[i] > 0)
                 return true;
         }
-        
+
         return false;
     }
 };

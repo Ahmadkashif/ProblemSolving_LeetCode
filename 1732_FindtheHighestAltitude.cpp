ec6bf0c091ccac0b8914fcbfe9af636ac7a588a9
Ahmadkashif <ahmadkashif983@gmail.com>
2022-11-23 11:08:30 +0500

    leetcode 1732

--- 1732_FindtheHighestAltitude.cpp
+++ 1732_FindtheHighestAltitude.cpp
@@ -1,42 +1,45 @@
 // class Solution {
 // public:
 //     int largestAltitude(vector<int>& gain) {
-        
+
 //         int max = 0, sum = 0;
-        
+
 //         for(int i = 0; i < gain.size(); i++){
 //             sum+=gain[i];
-            
+
 //             if(max < sum)
 //                 max = sum;
 //         }
-        
+
 //         return max;
 //     }
 // };
 
-class Solution {
+class Solution
+{
 public:
     int max = 0;
-    int acc(vector<int>& gain, int index){
-        
-        if(index==0){
-            if(max < gain[index])
+    int acc(vector<int> &gain, int index)
+    {
+
+        if (index == 0)
+        {
+            if (max < gain[index])
                 max = gain[index];
             return gain[index];
         }
-            
-        else{
+
+        else
+        {
             int sum = gain[index] + acc(gain, --index);
-            if(max < sum)
+            if (max < sum)
                 max = sum;
             return sum;
         }
-         
-        
     }
-    int largestAltitude(vector<int>& gain) {
-        acc( gain, gain.size()-1 );
+    int largestAltitude(vector<int> &gain)
+    {
+        acc(gain, gain.size() - 1);
         return max;
     }
 };

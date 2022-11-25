d08f70edfe595482eb07fbf6937c8ade34f835e8
Ahmadkashif <ahmadkashif983@gmail.com>
2022-11-25 12:14:59 +0500

    leetcode 1295

---
+++ 1295_ FindNumberswithEvenNumberofDigits.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int findNumbers(vector<int>& nums) {
+        int count = 0;
+        
+        for(int i = 0; i < nums.size(); i++){
+            
+            int d = 0;
+            
+            while( nums[i] > 0 ){
+                nums[i] = nums[i] / 10;
+                d++;
+            }
+            
+            if(d%2==0)
+                count++;
+        }
+        
+        return count;
+    }
+};

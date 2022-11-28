0f42891825cfa1950406878870a6fe89c7cd5068
Ahmadkashif <ahmadkashif983@gmail.com>
2022-11-28 12:07:19 +0500

    leetcode 66

--- 66_PlusOne.cpp
+++ 66_PlusOne.cpp
@@ -1,16 +1,20 @@
-class Solution {
+class Solution
+{
 public:
-    vector<int> plusOne(vector<int> &digits) {
+    vector<int> plusOne(vector<int> &digits)
+    {
         bool carry = true;
-        
-        for(int i=digits.size()-1; i >= 0 && carry; i--) {
-            carry = (++digits[i]%=10) == 0;
+
+        for (int i = digits.size() - 1; i >= 0 && carry; i--)
+        {
+            carry = (++digits[i] %= 10) == 0;
         }
 
-        if(carry) {
+        if (carry)
+        {
             digits.insert(digits.begin(), 1);
         }
-    
+
         return digits;
     }
 };

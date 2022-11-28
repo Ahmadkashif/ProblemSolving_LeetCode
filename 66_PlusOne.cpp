e5d5298e9f3b1688a6be9f6e088afc1d879b5376
Ahmadkashif <ahmadkashif983@gmail.com>
2022-11-28 12:07:09 +0500

    leetcode 66

--- 66_PlusOne.cpp
+++ 66_PlusOne.cpp
@@ -1,31 +1,3 @@
-#include <cmath>
-class Solution {
-public:
-    
-    vector<int> plusOne(vector<int>& d) {
-        
-        long num = 0;
-        long r = 0;
-        vector<int> res;
-        
-        for(int i = d.size()-1; i >= 0; i--){
-            num += d[i] * pow(10,r);
-            r++;
-        }
-        num++;
-        if(d[d.size()-1]!=9)
-            r/=10;
-        
-        while(num){
-            res.insert(res.begin(),num%10);
-            num/=10;
-        }
-        
-        return res;
-    }
-};
-
-
 class Solution {
 public:
     vector<int> plusOne(vector<int> &digits) {

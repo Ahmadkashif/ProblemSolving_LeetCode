2a95bcc6ac290de71264d94db167e875b1d98883
Ahmad Kashif <ahmadkashif983@gmail.com>
2022-11-30 12:41:51 +0500

    leetcode 1304

--- 1304_FindNUniqueIntegersSumuptoZero.cpp
+++ 1304_FindNUniqueIntegersSumuptoZero.cpp
@@ -1,21 +1,23 @@
-class Solution {
+class Solution
+{
 public:
-    vector<int> sumZero(int n) {
-        
+    vector<int> sumZero(int n)
+    {
+
         vector<int> res(n);
-        
+
         int n1 = n;
-        
-        for(int i = 0 ; i < n/2 ; i++){
-            
-            res[i] = -1 * n1/2;
-            
-            res[n-i-1] = n1/2;
-            
-            n1 = n1-2;
+
+        for (int i = 0; i < n / 2; i++)
+        {
+
+            res[i] = -1 * n1 / 2;
+
+            res[n - i - 1] = n1 / 2;
+
+            n1 = n1 - 2;
         }
-        
+
         return res;
     }
-    
 };

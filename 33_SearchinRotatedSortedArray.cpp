9feeca28ddf6c7bc3fc58e81899f2bc7cfc0675f
Ahmad Kashif <ahmadkashif983@gmail.com>
2022-12-14 16:32:54 +0500

    leetcode 33

--- 33_SearchinRotatedSortedArray.cpp
+++ 33_SearchinRotatedSortedArray.cpp
@@ -1,27 +1,33 @@
-class Solution {
+class Solution
+{
 public:
-    int search(vector<int>& nums, int t) {
-        int s = 0,          // 0
-        e = nums.size()-1,  // 6
-        mid;                // 3
-        if(nums.size()==1 && nums[0] ==t)
+    int search(vector<int> &nums, int t)
+    {
+        int s = 0,               // 0
+            e = nums.size() - 1, // 6
+            mid;                 // 3
+        if (nums.size() == 1 && nums[0] == t)
             return 0;
-        while( s <= e){
-            mid = (e - s)/ 2 + s;
-            if(nums[mid] == t){
+        while (s <= e)
+        {
+            mid = (e - s) / 2 + s;
+            if (nums[mid] == t)
+            {
                 return mid;
             }
-            else if ( nums[s] <= nums[mid]){
-                if(nums[s] <= t && t <= nums[mid])
-                    e = mid-1;
+            else if (nums[s] <= nums[mid])
+            {
+                if (nums[s] <= t && t <= nums[mid])
+                    e = mid - 1;
                 else
                     s = mid + 1;
             }
-            else if (nums[mid+1] <= nums[e]){
-                if(nums[mid+1] <= t && t <= nums[e])
+            else if (nums[mid + 1] <= nums[e])
+            {
+                if (nums[mid + 1] <= t && t <= nums[e])
                     s = mid + 1;
                 else
-                    e = mid -1;
+                    e = mid - 1;
             }
         }
         return -1;

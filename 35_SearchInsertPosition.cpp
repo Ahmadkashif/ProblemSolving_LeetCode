15b264b77026546858852d9a107c5b9f0c03345e
Ahmad Kashif <ahmadkashif983@gmail.com>
2022-12-15 13:20:50 +0500

    leetcode 35

--- 35_SearchInsertPosition.cpp
+++ 35_SearchInsertPosition.cpp
@@ -1,18 +1,21 @@
-class Solution {
+class Solution
+{
 public:
-    int searchInsert(vector<int>& nums, int t) {
+    int searchInsert(vector<int> &nums, int t)
+    {
 
-       int s = 0, e = nums.size()-1, mid = 0;
+        int s = 0, e = nums.size() - 1, mid = 0;
 
-       while( s <= e ){
-           mid = (e-s)/2 + s;
-           if(nums[mid]== t)
+        while (s <= e)
+        {
+            mid = (e - s) / 2 + s;
+            if (nums[mid] == t)
                 return mid;
-           else if(nums[mid] > t)
+            else if (nums[mid] > t)
                 e = mid - 1;
-           else 
+            else
                 s = mid + 1;
-       }
-       return s;
+        }
+        return s;
     }
 };

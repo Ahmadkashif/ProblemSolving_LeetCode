d234af2267f2a9e4e9705186ea5a37b1c017e9ee
Ahmad Kashif <ahmadkashif983@gmail.com>
2022-12-17 17:13:04 +0500

    leetcode 540

--- 540_SingleElementinaSortedArray.cpp
+++ 540_SingleElementinaSortedArray.cpp
@@ -1,21 +1,18 @@
-class Solution {
+class Solution
+{
 public:
-    int singleNonDuplicate(vector<int>& nums) {
+    int singleNonDuplicate(vector<int> &nums)
+    {
         int s = 0, e = nums.size() - 1, mid;
-        while(s<e){
-            mid = (e-s)/2+s;
-            if(mid%2==0){
-                if(nums[mid + 1] == nums[mid])
-                    s = mid + 1;
-                else
-                    e = mid;
-            }
-            else{
-                if(nums[mid - 1] == nums[mid])
-                    s = mid + 1;
-                else
-                    e = mid;
-            }
+        while (s < e)
+        {
+            mid = (e - s) / 2 + s;
+            if (mid % 2 == 0)
+                if (nums[mid + 1] == nums[mid])     s = mid + 1;
+                else                                e = mid;
+            else
+                if (nums[mid - 1] == nums[mid])     s = mid + 1;
+                else                                e = mid;
         }
         return nums[s];
     }

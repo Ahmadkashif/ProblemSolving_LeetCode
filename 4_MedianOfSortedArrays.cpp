53dee540a591b6bce18d7f9322f1eea2d110f17e
Ahmadkashif <ahmadkashif983@gmail.com>
2022-12-19 16:26:39 +0500

    leetcode 4

--- 4_MedianOfSortedArrays.cpp
+++ 4_MedianOfSortedArrays.cpp
@@ -3,21 +3,29 @@ class Solution
 public:
     double findMedianSortedArrays(vector<int> &nums1, vector<int> &nums2)
     {
-        double ans;
-        int m = nums1.size(), n = nums2.size();
+        int N1 = nums1.size();
+        int N2 = nums2.size();
+        if (N1 < N2)
+            return findMedianSortedArrays(nums2, nums1); // Make sure A2 is the shorter one.
 
-        // count the desired median position.
-        int i = 0, j = 0;
-        while (i < m && j < n)
+        int lo = 0, hi = N2 * 2;
+        while (lo <= hi)
         {
-            if (nums1[i] < nums2[j])
-                i++;
+            int mid2 = (lo + hi) / 2;  // Try Cut 2
+            int mid1 = N1 + N2 - mid2; // Calculate Cut 1 accordingly
+
+            double L1 = (mid1 == 0) ? INT_MIN : nums1[(mid1 - 1) / 2]; // Get L1, R1, L2, R2 respectively
+            double L2 = (mid2 == 0) ? INT_MIN : nums2[(mid2 - 1) / 2];
+            double R1 = (mid1 == N1 * 2) ? INT_MAX : nums1[(mid1) / 2];
+            double R2 = (mid2 == N2 * 2) ? INT_MAX : nums2[(mid2) / 2];
+
+            if (L1 > R2)
+                lo = mid2 + 1; // A1's lower half is too big; need to move C1 left (C2 right)
+            else if (L2 > R1)
+                hi = mid2 - 1; // A2's lower half too big; need to move C2 left.
             else
-                j++;
-
-            if ((m + n) % 2 == 0)
+                return (max(L1, L2) + min(R1, R2)) / 2; // Otherwise, that's the right cut.
         }
-
-        return ans;
+        return -1;
     }
 };

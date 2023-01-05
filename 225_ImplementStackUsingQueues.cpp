0dacabde069c837e30621c9d021842b571530cca
Ahmad Kashif <ahmadkashif983@gmail.com>
2023-01-05 23:50:00 +0500

    leetcode 225

---
+++ 225_ImplementStackUsingQueues.cpp
@@ -0,0 +1,35 @@
+class MyStack {
+public:
+    /** Initialize your data structure here. */
+    queue<int> q1;
+    queue<int> q2;
+    MyStack() {
+        
+    }
+    
+    /** Push element x onto stack. */
+    void push(int x) {
+        q2.push(x);
+        while(!q1.empty()){
+            q2.push(q1.front());    q1.pop();
+        }
+        swap(q1, q2);
+    }
+    
+    /** Removes the element on top of the stack and returns that element. */
+    int pop() {
+        int result = top();
+        q1.pop();
+        return result;
+    }
+    
+    /** Get the top element. */
+    int top() {
+        return q1.front();
+    }
+    
+    /** Returns whether the stack is empty. */
+    bool empty() {
+        return q1.empty();
+    }
+};

266128106f829ff081ff38f636cefafa1a51dd59
Ahmad Kashif <ahmadkashif983@gmail.com>
2023-04-22 18:18:52 +0500

    leetcode 2469

--- 2469_ConvertTheTemperature.cpp
+++ 2469_ConvertTheTemperature.cpp
@@ -1,6 +1,8 @@
-class Solution {
+class Solution
+{
 public:
-    vector<double> convertTemperature(double celsius) {
-        return {celsius + 273.15,celsius * 1.80 + 32.00};
+    vector<double> convertTemperature(double celsius)
+    {
+        return {celsius + 273.15, celsius * 1.80 + 32.00};
     }
 };

83e4db14c772a0ab296580ab20c30271c7813a81
Ahmad Kashif <ahmadkashif983@gmail.com>
2023-05-02 23:58:22 +0500

    leetcode 1822

--- 1822_SignOfProductOfArray.cpp
+++ 1822_SignOfProductOfArray.cpp
@@ -3,11 +3,12 @@ public:
     int arraySign(vector<int>& nums) {
         
         int result = 0;
-        long product = 1;
+        int product = 1;
         for(int n: nums){
             if(n==0)
                 return 0;
-            product *= n;
+
+            product = n > 0 ?  product : -1 * product ;
         }
 
         return product > 0 ? 1:-1;

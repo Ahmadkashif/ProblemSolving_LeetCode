7908f33f63e881a65febba979257ff1a01f4bd2a
Ahmad Kashif <ahmadkashif983@gmail.com>
2023-05-15 00:18:58 +0500

    leetcode 20

--- 20_ValidParenthesis.cpp
+++ 20_ValidParenthesis.cpp
@@ -1,16 +1,20 @@
-class Solution {
+class Solution
+{
 public:
-    bool isValid(string s) {
-        stack<char> st;  //taking stack for keep tracking the order of the brackets..
-        for(auto i:s)  //iterate over each and every elements
+    bool isValid(string s)
+    {
+        stack<char> st;
+        for (auto i : s)
         {
-            if(i=='(' or i=='{' or i=='[') st.push(i);  //if current element of the string will be opening bracket then we will just simply push it into the stack
-            else  //if control comes to else part, it means that current element is a closing bracket, so check two conditions  current element matches with top of the stack and the stack must not be empty...
+            if (i == '(' or i == '{' or i == '[')
+                st.push(i);
+            else
             {
-                if(st.empty() or (st.top()=='(' and i!=')') or (st.top()=='{' and i!='}') or (st.top()=='[' and i!=']')) return false;
-                st.pop();  //if control reaches to that line, it means we have got the right pair of brackets, so just pop it.
+                if (st.empty() or (st.top() == '(' and i != ')') or (st.top() == '{' and i != '}') or (st.top() == '[' and i != ']'))
+                    return false;
+                st.pop();
             }
         }
-        return st.empty();  //at last, it may possible that we left something into the stack unpair so return checking stack is empty or not..
+        return st.empty();
     }
 };

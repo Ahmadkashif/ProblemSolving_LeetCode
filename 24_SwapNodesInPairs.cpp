02d5716461a59aef8ece4afd3e994497de2f26b2
Ahmad Kashif <ahmadkashif983@gmail.com>
2023-05-16 23:59:31 +0500

    leetcode 24

--- 24_SwapNodesInPairs.cpp
+++ 24_SwapNodesInPairs.cpp
@@ -1,24 +1,28 @@
-ListNode* swapPairs(ListNode* head) {
-    // base case 
-    if(head == NULL){
+ListNode *swapPairs(ListNode *head)
+{
+    // base case
+    if (head == NULL)
+    {
         return NULL;
     }
-    
-    ListNode* forward = NULL;
-    ListNode* prev = NULL;
-    ListNode* curr = head;
+
+    ListNode *forward = NULL;
+    ListNode *prev = NULL;
+    ListNode *curr = head;
     int count = 0;
-    while(curr != NULL && count < 2){
-         forward = curr->next;
+    while (curr != NULL && count < 2)
+    {
+        forward = curr->next;
         curr->next = prev;
         prev = curr;
         curr = forward;
         count++;
     }
-    
-    if(forward != NULL){
+
+    if (forward != NULL)
+    {
         head->next = swapPairs(forward);
     }
-    
+
     return prev;
 }

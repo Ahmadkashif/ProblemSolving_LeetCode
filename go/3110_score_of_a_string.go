55780b82328e6c425fc9a9597a8b0c55e0fd46bd
Ahmad <57593864+Ahmadkashif@users.noreply.github.com>
2024-12-14 18:22:58 +0500

    Create 3110_score_of_a_string.go

---
+++ go/3110_score_of_a_string.go
@@ -0,0 +1,16 @@
+func scoreOfString(s string) int {
+    score := 0.0
+
+    byteArray := []byte(s)
+
+    for pos, char := range byteArray {
+        if pos < (len(byteArray) -1 ) {
+            score += math.Abs(byteArray[pos] - byteArray[pos+1])
+            char += char
+        } else {
+            break
+        }
+    }
+
+    return score
+}
